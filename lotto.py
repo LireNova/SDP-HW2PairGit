63adf27dcd439b7bd90b3f3031f19ecc43e3aa7f
thanachot <mo.thanachot_st@tni.ac.th>
2017-01-27 02:10:52 +0000

    First commit

---
+++ lotto.py
@@ -0,0 +1,11 @@
+from random import randint
+a=""
+for x in range(1, 5):
+    for i in range(0,3):
+        b=randint(0,9);
+        if x==0 and b==0 :
+            a+=str(0);
+        else:
+            a+=str(b);
+    print "Lotto",x,a[:];
+    a=""
